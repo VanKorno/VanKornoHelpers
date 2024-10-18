4e94661ebbb0c45a28ed97c63c8b84c4a13d1444
VanKorno <kornovandrey@gmail.com>
2024-10-18 21:23:31 +0300

    ...

--- VanKornoHelpers/build.gradle.kts
+++ VanKornoHelpers/build.gradle.kts
@@ -42,7 +42,7 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 }
 
-val versionTag = "1.1.4"
+val versionTag = "1.1.5"
 //val buildDirectory = layout.buildDirectory.get()
 
 publishing {    /* to ensure that the library is published correctly */
