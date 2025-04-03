32eab2597076500eef01bb0b6ca57385722b094b
VanKorno <kornnovandrey@gmail.com>
2025-04-04 00:51:18 +0300

    Removed ...release.aar

--- VanKornoHelpers/build.gradle.kts
+++ VanKornoHelpers/build.gradle.kts
@@ -42,7 +42,7 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 }
 
-val versionTag = "2.3"
+val versionTag = "2.4"
 //val buildDirectory = layout.buildDirectory.get()
 
 publishing {    /* to ensure that the library is published correctly */
