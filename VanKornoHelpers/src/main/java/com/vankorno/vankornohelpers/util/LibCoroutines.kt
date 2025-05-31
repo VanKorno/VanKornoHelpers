d10e208793d3835c7cd1d29cf04b0876b6ad5ceb
VanKorno <71512024+vankorno@users.noreply.github.com>
2025-05-31 06:12:25 +0300

    Updated. Added LibCoroutines.

---
+++ VanKornoHelpers/src/main/java/com/vankorno/vankornohelpers/util/LibCoroutines.kt
@@ -0,0 +1,36 @@
+package com.vankorno.vankornohelpers.util
+
+import com.vankorno.vankornohelpers.eLog
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.supervisorScope
+
+object LibCoroutines {
+    val IO = CoroutineScope(Dispatchers.IO + SupervisorJob())
+    val Default = CoroutineScope(Dispatchers.Default + SupervisorJob())
+    val Main = CoroutineScope(Dispatchers.Main + SupervisorJob())
+    
+    
+    /** A special handler that catches uncaught exceptions in my coroutines **/
+    val logHandler = CoroutineExceptionHandler { _, t ->
+        eLog("AppCoroutines", "Unhandled: ${t.message}", t)
+    }
+    
+    
+    /**
+     * A helper function to run a group of coroutine tasks where if one fails,
+     * it won't cancel the others automatically, but still lets me catch errors cleanly.
+     */
+    suspend fun <T> runSafe(                                    block: suspend CoroutineScope.()->T
+    ): T = supervisorScope {
+        try {
+            block()
+        } catch (e: Exception) {
+            eLog("AppCoroutines", "runSafe error: ${e.message}", e)
+            throw e
+        }
+    }
+    
+}
