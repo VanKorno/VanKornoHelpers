21fa4f83fc9ec2ffb95a044d7d7934af90d04aef
VanKorno <71512024+vankorno@users.noreply.github.com>
2025-07-22 18:48:23 +0300

    Added UKR_Slang

--- VanKornoHelpers/src/main/java/com/vankorno/vankornohelpers/values/LibConstants.java
+++ VanKornoHelpers/src/main/java/com/vankorno/vankornohelpers/values/LibConstants.java
@@ -3,7 +3,7 @@ package com.vankorno.vankornohelpers.values;
 public class LibConstants {
     public static final String
         //  L A N G U A G E
-LangAuto="auto",        ENG="en",       UKR="uk"
+LangAuto="auto",        ENG="en",       UKR="uk",       UKR_Slang = "ua_slang"
 ;
 
 
