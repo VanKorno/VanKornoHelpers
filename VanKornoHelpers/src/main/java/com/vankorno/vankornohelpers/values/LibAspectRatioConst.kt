db8d24f6d804d2472bdbb992fb9c0999c1a28b62
VanKorno <71512024+vankorno@users.noreply.github.com>
2025-07-30 17:27:15 +0300

    Added LibAspectRatioConst.kt

---
+++ VanKornoHelpers/src/main/java/com/vankorno/vankornohelpers/values/LibAspectRatioConst.kt
@@ -0,0 +1,10 @@
+package com.vankorno.vankornohelpers.values
+
+const val AspectRatioFloat2by3 = 0.6666667f
+const val AspectRatioFloat3by2 = 1.5f
+
+const val AspectRatioFloat16by9 = 1.7777778f
+const val AspectRatioFloat9by16 = 0.5625f
+
+const val AspectRatioFloat4by3 = 1.3333334f
+const val AspectRatioFloat3by4 = 0.75f
