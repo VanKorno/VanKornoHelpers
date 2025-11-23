d083bca887a8b00bc35464eb6637953ce16dc9ec
VanKorno <kornovandrey@gmail.com>
2025-11-23 19:32:14 +0200

    Add plain colors

--- VanKornoHelpers/src/main/java/com/vankorno/vankornohelpers/values/LibColors.java
+++ VanKornoHelpers/src/main/java/com/vankorno/vankornohelpers/values/LibColors.java
@@ -4,7 +4,7 @@ public class LibColors {
     public static final int
 Transparent=0x00000000,
 White=0xFFFFFFFF, WhiteTransp=0x80FFFFFF,
-Black=0xFF000000, BlackT30=0x4D000000, BlackT90=0xE6000000,
+BlackT30=0x4D000000, BlackT90=0xE6000000,
 GreyText=0xFF4D4D4D, PlainDarkGrey=0xFF1B1B1B,
 StandardBtnColor=0xFFE6E6E6,
 
@@ -24,5 +24,37 @@ GlassBrown = 0x59815214,
 GlassYellow = 0x59FFD416,
 GlassOrange = 0x59FF4E22,
 GlassAvocado = 0x59E8FD26,
-GlassCyan = 0x5926FDB2;
+GlassCyan = 0x5926FDB2,
+
+PlainBlack = 0xFF000000,
+PlainGrey = 0xFF5A5A5A,
+PlainViolet = 0xFF352652,
+PlainBlueDark = 0xFF25306C,
+PlainBlueLight = 0xFF153E5E,
+PlainMagenta = 0xFF5A1766,
+PlainPink = 0xFF641E38,
+PlainRed = 0xFF4F2215,
+PlainGreenDark = 0xFF142F2A,
+PlainGreenLight = 0xFF3F522B,
+PlainOlive = 0xFF31381B,
+PlainBrown = 0xFF41311B,
+PlainYellow = 0xFF6C5D1C,
+PlainOrange = 0xFF6C2F20,
+PlainAvocado = 0xFF656C21,
+PlainCyan = 0xFF216C52
+
+;
+    
+    public static final Integer[][]
+
+GlassColors = {
+    { GlassGrey,  GlassYellow, GlassAvocado, GlassGreenLight, GlassCyan,      GlassBlueDark, GlassPink,    GlassOrange },
+    { GlassBlack, GlassBrown,  GlassOlive,   GlassGreenDark,  GlassBlueLight, GlassViolet,   GlassMagenta, GlassRed }
+},
+
+PlainColors = {
+    { PlainGrey,  PlainYellow, PlainAvocado, PlainGreenLight, PlainCyan,      PlainBlueDark, PlainPink,    PlainOrange },
+    { PlainBlack, PlainBrown,  PlainOlive,   PlainGreenDark,  PlainBlueLight, PlainViolet,   PlainMagenta, PlainRed }
+}
+;
 }
