aff75a0de2851411349afb0f5a4f2bcb48f9f52e
VanKorno <kornovandrey@gmail.com>
2025-11-28 01:40:13 +0200

    Clipboard

--- VanKornoHelpers/src/main/java/com/vankorno/vankornohelpers/clipboard/LibClipboard.kt
+++ VanKornoHelpers/src/main/java/com/vankorno/vankornohelpers/clipboard/LibClipboard.kt
@@ -3,28 +3,8 @@ package com.vankorno.vankornohelpers.clipboard
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 
 object LibClipboard {
-    
-    private val listeners = mutableSetOf<(String) -> Unit>()
-    
-    private val _clipboardFlow = MutableStateFlow("")
-    val clipboardFlow: StateFlow<String> get() = _clipboardFlow
-    
-    
-    fun addListener(l: (String) -> Unit) { listeners += l }
-    
-    fun removeListener(l: (String) -> Unit) { listeners -= l }
-    
-    
-    private fun notifyAll(                                                           txt: String
-    ) {
-        _clipboardFlow.value = txt
-        listeners.forEach { it(txt) }
-    }
-    
     
     fun getClipboard(                                                            context: Context
     ): String {
@@ -39,7 +19,6 @@ object LibClipboard {
     ) {
         val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
         clipboard.setPrimaryClip(ClipData.newPlainText("simple text", txt))
-        notifyAll(txt)
     }
     
     
@@ -47,37 +26,8 @@ object LibClipboard {
     ) {
         val clipboardManager = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
         clipboardManager.setPrimaryClip(ClipData.newPlainText("", ""))
-        notifyAll("")
     }
     
-    
-    
-    private var sysListener: ClipboardManager.OnPrimaryClipChangedListener? = null
-    
-    
-    fun attachSystemListener(                                                    context: Context
-    ) {
-        if (sysListener != null) return
-        
-        val clipboardManager = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-        
-        val listener = ClipboardManager.OnPrimaryClipChangedListener {
-            notifyAll(getClipboard(context))
-        }
-        sysListener = listener
-        clipboardManager.addPrimaryClipChangedListener(listener)
-        
-        notifyAll(getClipboard(context))
-    }
-    
-    
-    fun detachSystemListener(                                                    context: Context
-    ) {
-        val listener = sysListener ?: return
-        val clipboardManager = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-        clipboardManager.removePrimaryClipChangedListener(listener)
-        sysListener = null
-    }
     
     
     
