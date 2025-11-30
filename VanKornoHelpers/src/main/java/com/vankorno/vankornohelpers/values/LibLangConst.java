d22e95dedb05432fc55a3417d54c22d742dd06ab
VanKorno <kornovandrey@gmail.com>
2025-11-30 22:20:17 +0200

    Language stuff

---
+++ VanKornoHelpers/src/main/java/com/vankorno/vankornohelpers/values/LibLangConst.java
@@ -0,0 +1,57 @@
+package com.vankorno.vankornohelpers.values;
+
+public class LibLangConst {
+    public static final String
+        LangAuto = "auto",
+        ENG = "en",
+        UKR = "uk",
+        SPA = "es",    // Spanish
+        FRA = "fr",    // French
+        DEU = "de",    // German
+        ITA = "it",    // Italian
+        POR = "pt",    // Portuguese
+        POL = "pl",    // Polish
+        NLD = "nl",    // Dutch
+        SWE = "sv",    // Swedish
+        NOR = "no",    // Norwegian
+        DAN = "da",    // Danish
+        FIN = "fi",    // Finnish
+        CZE = "cs",    // Czech
+        SLK = "sk",    // Slovak
+        SLV = "sl",    // Slovenian
+        RON = "ro",    // Romanian
+        BUL = "bg",    // Bulgarian
+        HRV = "hr",    // Croatian
+        SRP = "sr",    // Serbian
+        EST = "et",    // Estonian
+        LAV = "lv",    // Latvian
+        LIT = "lt",    // Lithuanian
+        GRE = "el",    // Greek
+        TUR = "tr",    // Turkish
+        ARA = "ar",    // Arabic
+        HEB = "he",    // Hebrew
+        CHI = "zh",    // Chinese
+        JPN = "ja",    // Japanese
+        KOR = "ko",    // Korean
+        HIN = "hi",    // Hindi
+        IND = "id",    // Indonesian
+        VIE = "vi",    // Vietnamese
+        THA = "th",    // Thai
+        FIL = "fil",   // Filipino
+        //HUN = "hu",    // Hungarian
+        
+        UsLangTag = "en-US",
+        UkrLangTag = "uk-UA"
+    ;
+
+
+
+
+
+
+
+
+
+
+
+}
